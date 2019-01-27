48c26374c4208ae71f3bbe91f06fecf757726ef3
shu-matsu-uotza <matsuyamashuuhei@matsuyamashuuhei-no-MacBook-Air.local>
2019-01-28 00:21:59 +0900

    EDIT-update_method-groups_controller-because-an_error

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -24,9 +24,10 @@ class GroupsController < ApplicationController
 
   def update
     if @group.update(group_params)
-      redirect_to group_messages_path(@group), notice 'グループを編集しました。'
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました。'
     else
       render :edit
+    end
   end
 
   private
