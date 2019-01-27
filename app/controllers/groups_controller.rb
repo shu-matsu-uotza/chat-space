df28614c9a6e9dd12c234869cd47b80c14f27628
shu-matsu-uotza <matsuyamashuuhei@matsuyamashuuhei-no-MacBook-Air.local>
2019-01-27 20:22:38 +0900

    def-groups_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,24 @@
 class GroupsController < ApplicationController
+
+  def new
+  end
+
+  def create(groups_permit)
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  private
+
+  def groups_permit
+    params.permit()
+  end
+
 end
