f548624038f1a645de95d626bbd2c51a57dcda01
shu-matsu-uotza <matsuyamashuuhei@matsuyamashuuhei-no-MacBook-Air.local>
2019-02-11 17:54:47 +0900

    EDIT .done method

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -51,4 +51,10 @@ $(function(){
       processData: false,
       contentType: false
     })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      $('form')[0].reset();
+    })
 });
