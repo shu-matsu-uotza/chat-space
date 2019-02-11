c0ffc803a2f86321e7384f05c4d800b52ccdcf07
shu-matsu-uotza <matsuyamashuuhei@matsuyamashuuhei-no-MacBook-Air.local>
2019-02-12 00:20:13 +0900

    EDIT index.js, function appendUser

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,4 +1,15 @@
 $(function() {
+
+function appendUser(user) {
+  var html = `<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">
+                  ${ user.name }
+                </p>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</a>
+              </div>`
+  search_list.append(html);
+}
+
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
 
