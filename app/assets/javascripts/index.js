0b5defd2617ea8672e8a46c8a1b1e93b9b0b69d7
shu-matsu-uotza <matsuyamashuuhei@matsuyamashuuhei-no-MacBook-Air.local>
2019-02-12 00:18:12 +0900

    EDIT index.js, .done method

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -8,4 +8,16 @@ $(function() {
       data: { keyword: input },
       dataType: 'json'
     })
+
+    .done(function(users) {
+      $("#user-search-result").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      }
+      else {
+        appendNoUser("一致するユーザーは見つかりませんでした")
+      }
+    })
 });
